77fc4e866161f49ead6845e7d65c186a76fad10d
fluttr <fluttershash@gmail.com>
2022-11-07 21:39:23 +0300

    Update parse_porkbun.js
    It works, finally.

--- parse_porkbun.js
+++ parse_porkbun.js
@@ -1,21 +1,21 @@
 // https://porkbun.com
+// in developer console out all prices for domain search sorted by renew price ascending
 
-let domainNames = $x("//div[@class='row searchResultRowTable']//div[contains(@class, 'availableDomain')]/text()") 
-
-let rawFirstPrices = $x("//div[@class='row searchResultRowTable']/div[contains(@class,'searchResultRowCell')]/span[@class='childContent']/text()") 
-function containsDigit(val){return null != val.match("\\d")}
-let firstYearPrices = rawFirstPrices.map(v => v.textContent).filter(containsDigit)
- 
-/// а теперь по-нормальному
-let domainNames = $x("//div[@class='row searchResultRowTable']//small[@class='text-muted renewsAtContainer']/../../../div[contains(@class, 'availableDomain')]/text()").map(v => v.textContent)
-let firstYearPrices = $x("//div[@class='row searchResultRowTable']//small[@class='text-muted renewsAtContainer']/../text()").map(v => v.textContent)
-let renewPrices = $x("//div[@class='row searchResultRowTable']//small[@class='text-muted renewsAtContainer']/text()").map(v => v.textContent)
-let parsed = []
-for(let i = 0; i < domainNames.length; i++){
-    let n = domainNames[i].match("[\\w\\.]+")[0];
-    let fp = parseFloat(firstYearPrices[i].match("[\\d\\.]+") == null? NaN : firstYearPrices[i].match("[\\d\\.]+")[0]);
-    let rp = parseFloat(renewPrices[i].match("[\\d\\.]+") == null? NaN : renewPrices[i].match("[\\d\\.]+"));
+var priceNodes = $x("//div[@class='row searchResultRowTable']//small[@class='text-muted renewsAtContainer']/../../../div[contains(@class, 'availableDomain')]/..")
+var parsed = []
+priceNodes.forEach((node, a, b) => {
+    let rawName = node.getElementsByClassName("availableDomain")[0].textContent
+    let renewPrice = node.getElementsByClassName("renewsAtContainer")[0].textContent
+    let rawCurrentPrice = node.querySelector("span.childContent").innerText.replaceAll('\n','')
+    let m = rawCurrentPrice.match("(?<strikedPrice>\\$[\\d\\.]+)?\\s?(?<currentPrice>\\$[\\d\\.]+)")
+    let currentPrice = m.groups['currentPrice']
+    //console.log(`rawName: ${rawName} rawRenewPrice: ${renewPrice} rawCurrentPrice: ${currentPrice}`)
+    let n = rawName.match("[\\w\\.]+")[0];
+    m = currentPrice.match("[\\d\\.]+")
+    let fp = parseFloat( null == m? NaN : m[0])
+    m = renewPrice.match("[\\d\\.]+")
+    let rp = parseFloat(null == m? NaN : m[0])
     parsed.push({name: n, firstPrice: fp, renewPrice: rp});
-}
+})
 parsed.sort( (a,b) => {if(a.renewPrice < b.renewPrice){return -1;}else if(a.renewPrice == b.renewPrice){return 0;}else{return 1}} );
 parsed.forEach((cur,i,arr) => {console.log(`name: ${cur.name} firstPrice: ${cur.firstPrice} renewPrice: ${cur.renewPrice}`)})
